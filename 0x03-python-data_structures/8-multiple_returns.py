077a42895f89b680c8b94c1205d713d8151a97a4
San-nimona <kaybryanmc@gmail.com>
2023-09-17 09:07:43 -0700

    task 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,11 +3,10 @@ def multiple_returns(sentence):
     '''
     A function that returns len of str and 1st char
     '''
-    if sentence:
-        str_len = len(sentence)
-        if len(sentence) == 0:
-            first_char = None
-        else:
-            first_char = sentence[0]
-        tup = (str_len, first_char)
-        return (tup)
+    str_len = len(sentence)
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    tup = (str_len, first_char)
+    return (tup)
