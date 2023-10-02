53251249ad878532f3cabb04afa2785880a97218
San-nimona <kaybryanmc@gmail.com>
2023-10-02 02:08:59 -0700

    correcting the pycodestyle requirement

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
 ''' A Module that creates a Square object '''
+
 
 class Square:
     ''' Creating an Object template '''
 
-    def __init__(self, size = 0):
-        '''
+    def __init__(self, size=0):
+        """
             The init method initializes class instance
 
         @self:
@@ -13,7 +15,7 @@ class Square:
 
         @size:
             size of square - a +ve integer
-        '''
+        """
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
