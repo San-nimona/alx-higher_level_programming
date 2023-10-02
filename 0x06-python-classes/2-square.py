595dc79df45e5da1d72553c5afc983036c464770
San-nimona <kaybryanmc@gmail.com>
2023-10-02 01:54:30 -0700

    adding task 2-square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 ''' A Module that creates a Square object '''
 
-class Square;
-''' Creating an Object template '''
+class Square:
+    ''' Creating an Object template '''
 
     def __init__(self, size = 0):
         '''
@@ -14,10 +14,8 @@ class Square;
         @size:
             size of square - a +ve integer
         '''
-        if type(size) is int:
-            if size < 0:
-                raise ValueError('Size must be >= 0')
-            else:
-                self.__size = size
-        else:
-            raise TypeError('Size must be an integer')
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
