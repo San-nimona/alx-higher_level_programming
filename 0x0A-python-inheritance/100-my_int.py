d3bdc77fc5731510a15f37917976b8e67234e46a
San-nimona <kaybryanmc@gmail.com>
2023-10-09 14:09:36 -0700

    task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -7,9 +7,11 @@ class MyInt(int):
     def __init__(self, value):
         """Instantiation with value"""
         self.value = value
+
     def __eq__(self, value):
         """Reimplimenting equality"""
         return self.value != value
+
     def __ne__(self, value):
         """Reimplimenting equality"""
         return self.value == value
