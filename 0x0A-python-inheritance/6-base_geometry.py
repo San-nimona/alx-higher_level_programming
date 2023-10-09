deb6c6a9028534074c153afe8fc542fc9fead003
San-nimona <kaybryanmc@gmail.com>
2023-10-09 10:12:29 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""6-base_geometry.py"""
+
+
+class BaseGeometry:
+    """An improvement of BaseGeometry"""
+    def area(self):
+        """Raises an Exception"""
+        raise Exception('area() is not implemented')
