94300f8650e5ffee47be7ec26309475055d1baad
San-nimona <kaybryanmc@gmail.com>
2023-10-09 08:58:00 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,9 +7,5 @@ class MyList(list):
         Prints a list in a sorted ascending order"""
     def print_sorted(self):
         """Prints the list in a sorte order"""
-        if not isinstance (self, list):
-            raise TypeError('Must be a list')
-        for i in self:
-            if not isinstance(i, int):
-                raise TypeError('element must be int')
-        print(sorted(self))
+        new_list = sorted(self)
+        print(new_list)
