08f9951f8f10b89eb56efb7eda8ad31407f2f6c7
San-nimona <kaybryanmc@gmail.com>
2023-10-10 12:29:41 -0700

    task 11

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""9-student.py"""
+
+
+class Student:
+    """
+    A class that defines a student by: firts and last name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student
+        """
+
+        if (type(attrs) == list) and\
+                all (type(i) == str for i in attrs):
+            return{i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
