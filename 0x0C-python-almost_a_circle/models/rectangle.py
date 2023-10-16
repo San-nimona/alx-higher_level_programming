5b06584556b88369e404282fabaedc7906f0b27e
San-nimona <kaybryanmc@gmail.com>
2023-10-16 16:12:06 -0700

    adding all my tasks

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,6 +3,8 @@
 
 
 from models.base import Base
+
+
 class Rectangle(Base):
     """Inherits from Base class"""
     def validate(self, name, value):
@@ -11,10 +13,11 @@ class Rectangle(Base):
             raise TypeError('{} must be an integer'.format(name))
         if name == "x" or name == "y":
             if value < 0:
-                raise ValueError('{} must >= 0'.format(name))     
+                raise ValueError('{} must >= 0'.format(name))
         else:
             if value <= 0:
-                raise  ValueError('{} must be > 0'.format(name))
+                raise ValueError('{} must be > 0'.format(name))
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Instatiation"""
         self.validate("width", width)
@@ -31,6 +34,7 @@ class Rectangle(Base):
     def width(self):
         """Getter for width"""
         return self.__width
+
     @width.setter
     def width(self, value):
         self.validate("width", value)
@@ -40,6 +44,7 @@ class Rectangle(Base):
     def height(self):
         """Getter for height"""
         return self.__width
+
     @height.setter
     def height(self, value):
         self.validate("height", value)
@@ -49,6 +54,7 @@ class Rectangle(Base):
     def x(self):
         """Getter for x"""
         return self.__x
+
     @x.setter
     def x(self, value):
         """Setter for x"""
@@ -58,6 +64,7 @@ class Rectangle(Base):
     def area(self):
         """Computes the area"""
         return self.__width * self.__height
+
     def display(self):
         """Prints the Rectangle instance"""
         for i in range(self.__y):
@@ -65,30 +72,32 @@ class Rectangle(Base):
             print("")
         for x in range(self.__height):
             """Then print empty spaces first before the #'s"""
-            print(" "* self.__x, end="")
+            print(" " * self.__x, end="")
             print("#" * self.__width)
+
     def __str__(self):
-        """Modifying str"""
-        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.__x, self.__y, self.__width, self.__height)
+        """"overriding __str__()"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
+
     def update(self, *args, **kwargs):
-        """it updats the class"""
-        n = 0
-        if args and len(args) != 0:
-            for i in args:
-                names = ["id", "width", "height", "x", "y"]
-                self.validate(names[n], i)
-                n += 1
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) > 1:
-                self.__width = args[1]
-            if len(args) > 2:
-                self.__height = args[2]
-            if len(args) > 3:
-                self.__x = args[3]
-            if len(args) > 4:
-                self.__y = args[4]
+        """"assigns an argument to each attribute"""
+
+        up = ["id", "width", "height", "x", "y"]
+        if (args):
+            for i in range(len(args)):
+                setattr(self, up[i], args[i])
+
         else:
-            for name, value in kwargs.items():
-                    self.validate(name, value)
-            self.__dict__.update(kwargs)
+            for k in kwargs:
+                setattr(self, k, kwargs[k])
+
+    def to_dictionary(self):
+        """Returns dict representation of a Rectangle"""
+        return {
+            "x": self.x,
+            "y": self.__y,
+            "id": self.id,
+            "height": self.__height,
+            'width': self.__width,
+        }
